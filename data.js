1c0d0844ddbd590daf55fe5cc1cb4fa7dd881473
Robinson Chacko <chacko@adobe.com>
2017-08-29 19:20:04 +0530

    testing json response

--- data.js
+++ data.js
@@ -10,21 +10,45 @@ var callAPI = function(options, callback) {
     request(options)  
       .then(function (data) {
         // Request was successful, use the response object at will
-        // console.log(data.hits.hits);
         var converter = JM.makeConverter({
-          // products:['hits', JM.map('_source.product', function(input){
-            
-
-          // })],
           products: ['hits', JM.map({
               name: '_source.product',
-              price:'_source.price'
+              link:'_source.dre_landing_page_url',
+              price:'_source.discounted_price',
+              image:'_source.search_image'
+
           })]
         });
         
        var result = converter(data.hits);
-        
-       console.log(result); // should be {name: 'John'} 
+       console.log(result);
+       var attachments = {
+                          "attachments":[] 
+                        };
+      attachments.attachments.push({
+                    "fallback": "Required plain-text summary of the attachment.",
+                    "color": "#36a64f",
+                    "pretext": "Optional text that appears above the attachment block",
+                    "author_name": "Bobby Tables",
+                    "author_link": "http://flickr.com/bobby/",
+                    "author_icon": "http://flickr.com/icons/bobby.jpg",
+                    "title": "Slack API Documentation",
+                    "title_link": "https://api.slack.com/",
+                    "text": "Optional text that appears within the attachment",
+                    "fields": [
+                        {
+                            "title": "Priority",
+                            "value": "High",
+                            "short": false
+                        }
+                    ],
+                    "image_url": "http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006222-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-1.jpg",
+                    "thumb_url": "http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006222-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-1.jpg",
+                    "footer": "Slack API",
+                    "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+                    "ts": 123456789
+                })   ;             
+
       //   var attachments = {
       //     "attachments": [
       //         {
@@ -52,7 +76,7 @@ var callAPI = function(options, callback) {
       //         }
       //     ]
       // }
-        return callback('heyyy');
+        return callback(attachments);
       })
       .catch(function (err) {
         // Something bad happened, handle the error
