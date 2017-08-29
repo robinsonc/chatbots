2308bd18252af4a9fd6c6c5e4419506ad0b8cf86
Robinson Chacko <chacko@adobe.com>
2017-08-29 18:16:48 +0530

    testing attchments

--- data.js
+++ data.js
@@ -23,32 +23,14 @@ var callAPI = function(options, callback) {
         
        console.log(result); // should be {name: 'John'} 
         var attachments = {
-                            "attachments": [
-                                {
-                                    "fallback": "Required plain-text summary of the attachment.",
-                                    "color": "#36a64f",
-                                    "pretext": "Optional text that appears above the attachment block",
-                                    "author_name": "Bobby Tables",
-                                    "author_link": "http://flickr.com/bobby/",
-                                    "author_icon": "http://flickr.com/icons/bobby.jpg",
-                                    "title": "Slack API Documentation",
-                                    "title_link": "https://api.slack.com/",
-                                    "text": "Optional text that appears within the attachment",
-                                    "fields": [
-                                        {
-                                            "title": "Priority",
-                                            "value": "High",
-                                            "short": false
-                                        }
-                                    ],
-                                    "image_url": "http://my-website.com/path/to/image.jpg",
-                                    "thumb_url": "http://example.com/path/to/thumb.png",
-                                    "footer": "Slack API",
-                                    "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-                                    "ts": 123456789
-                                }
-                            ]
-                        };
+          attachments: [
+              {
+                  contentType: 'image/jpg',
+                  contentUrl: 'http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006222-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-1.jpg',
+                  name: 'Men Tshirt'
+              }
+          ]
+      }
         return callback(attachments);
       })
       .catch(function (err) {
