3c63f254d3c74115f81bacbbbf371d46ba42bef6
Robinson Chacko <chacko@adobe.com>
2017-08-29 19:02:56 +0530

    json mapper

--- data.js
+++ data.js
@@ -12,67 +12,47 @@ var callAPI = function(options, callback) {
         // Request was successful, use the response object at will
         // console.log(data.hits.hits);
         var converter = JM.makeConverter({
-          all_products: ['hits', JM.map('_source.product')],
           // products:['hits', JM.map('_source.product', function(input){
-          //   return {"name":input.product, "price":input.price};
+            
 
-          // })]
+          // })],
+          products: ['hits', JM.map({
+              name: '_source.product',
+              price:'_source.price'
+          })]
         });
         
        var result = converter(data.hits);
         
        console.log(result); // should be {name: 'John'} 
-        var attachments = {
-          "attachments": [
-              {
-                  "fallback": "Required plain-text summary of the attachment.",
-                  "color": "#36a64f",
-                  "pretext": "Optional text that appears above the attachment block",
-                  "author_name": "Bobby Tables",
-                  "author_link": "http://flickr.com/bobby/",
-                  "author_icon": "http://flickr.com/icons/bobby.jpg",
-                  "title": "Slack API Documentation",
-                  "title_link": "https://api.slack.com/",
-                  "text": "Optional text that appears within the attachment",
-                  "fields": [
-                      {
-                          "title": "Priority",
-                          "value": "High",
-                          "short": false
-                      }
-                  ],
-                  "image_url": "http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006222-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-1.jpg",
-                  "thumb_url": "http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006222-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-1.jpg",
-                  "footer": "Slack API",
-                  "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-                  "ts": 123456789
-              },
-              {
-                "fallback": "Required plain-text summary of the attachment.",
-                "color": "#36a64f",
-                "pretext": "second one",
-                "author_name": "Robby Abraham Chacko",
-                "author_link": "http://flickr.com/bobby/",
-                "author_icon": "http://flickr.com/icons/bobby.jpg",
-                "title": "Adidas t shirt",
-                "title_link": "https://api.slack.com/",
-                "text": "Optional text that appears within the attachment",
-                "fields": [
-                    {
-                        "title": "Priority",
-                        "value": "High",
-                        "short": false
-                    }
-                ],
-                "image_url": "http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006200-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-2.jpg",
-                "thumb_url": "http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006222-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-2.jpg",
-                "footer": "Slack API",
-                "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-                "ts": 123456789
-            }
-          ]
-      }
-        return callback(attachments);
+      //   var attachments = {
+      //     "attachments": [
+      //         {
+      //             "fallback": "Required plain-text summary of the attachment.",
+      //             "color": "#36a64f",
+      //             "pretext": "Optional text that appears above the attachment block",
+      //             "author_name": "Bobby Tables",
+      //             "author_link": "http://flickr.com/bobby/",
+      //             "author_icon": "http://flickr.com/icons/bobby.jpg",
+      //             "title": "Slack API Documentation",
+      //             "title_link": "https://api.slack.com/",
+      //             "text": "Optional text that appears within the attachment",
+      //             "fields": [
+      //                 {
+      //                     "title": "Priority",
+      //                     "value": "High",
+      //                     "short": false
+      //                 }
+      //             ],
+      //             "image_url": "http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006222-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-1.jpg",
+      //             "thumb_url": "http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006222-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-1.jpg",
+      //             "footer": "Slack API",
+      //             "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
+      //             "ts": 123456789
+      //         }
+      //     ]
+      // }
+        return callback('heyyy');
       })
       .catch(function (err) {
         // Something bad happened, handle the error
