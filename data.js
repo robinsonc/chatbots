d96f2857b9a823030caefb9acf09a9792f073a94
Robinson Chacko <chacko@adobe.com>
2017-08-29 19:28:21 +0530

    test

--- data.js
+++ data.js
@@ -13,41 +13,22 @@ var callAPI = function(options, callback) {
         var converter = JM.makeConverter({
           products: ['hits', JM.map({
               name: '_source.product',
-              link:'_source.dre_landing_page_url',
+              link: 'https://www.myntra.com/'+'_source.dre_landing_page_url',
               price:'_source.discounted_price',
               image:'_source.search_image'
-
           })]
         });
         
        var result = converter(data.hits);
        console.log(result);
-       var attachments = {
-                          "attachments":[] 
-                        };
-      attachments.attachments.push({
-                    "fallback": "Required plain-text summary of the attachment.",
-                    "color": "#36a64f",
-                    "pretext": "Optional text that appears above the attachment block",
-                    "author_name": "Bobby Tables",
-                    "author_link": "http://flickr.com/bobby/",
-                    "author_icon": "http://flickr.com/icons/bobby.jpg",
-                    "title": "Slack API Documentation",
-                    "title_link": "https://api.slack.com/",
-                    "text": "Optional text that appears within the attachment",
-                    "fields": [
-                        {
-                            "title": "Priority",
-                            "value": "High",
-                            "short": false
-                        }
-                    ],
-                    "image_url": "http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006222-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-1.jpg",
-                    "thumb_url": "http://myntra.myntassets.com/assets/images/1729487/2017/1/25/11485347006222-WROGN-Men-Olive-Printed-V-Neck-T-Shirt-4691485347005939-1.jpg",
-                    "footer": "Slack API",
-                    "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-                    "ts": 123456789
-                })   ;             
+       var attachments = { "attachments":[] };
+       for (var key in result['products']) {
+         if (result['products'].hasOwnProperty(key)) {
+           var element = result['products'][key];
+           console.log(element);
+         }
+       }
+                  
 
       //   var attachments = {
       //     "attachments": [
@@ -76,7 +57,7 @@ var callAPI = function(options, callback) {
       //         }
       //     ]
       // }
-        return callback(attachments);
+        return callback('heyy');
       })
       .catch(function (err) {
         // Something bad happened, handle the error
