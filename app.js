2e48bce53a7f37e18bb023c3347fededbe6af00e
EC2 Default User <ec2-user@ip-172-31-40-203.ec2.internal>
2017-09-22 06:03:33 +0000

    Added user session using mongodb before first demo

--- app.js
+++ app.js
@@ -17,20 +17,37 @@
 require('dotenv').load();
 
 var wit = require('botkit-witai')({
-    accessToken: process.env.WIT_TOKEN,
-    minConfidence: 0.6,
-    logLevel: 'error'
+  accessToken: process.env.WIT_TOKEN,
+  minConfidence: 0.6,
+  logLevel: 'error'
 });
 
-const {Wit, log} = require('node-wit');
- 
+const {
+  Wit,
+  log
+} = require('node-wit');
+
 const client = new Wit({
   accessToken: process.env.WIT_TOKEN,
   //logger: new log.Logger(log.DEBUG) // optional 
 });
 
+
+var MongoClient = require('mongodb').MongoClient,
+  assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/co2';
+
+// Use connect method to connect to the server
+var database = MongoClient.connect(url);
+
 var id;
-module.exports = function(app) {
+var controller;
+var queryString = '';
+var global_product;
+
+module.exports = function (app) {
   if (process.env.USE_SLACK) {
     var Slack = require('./bot-slack');
     Slack.controller.middleware.receive.use(wit.receive);
@@ -50,107 +67,196 @@ module.exports = function(app) {
     BotFramework.controller.middleware.receive.use(wit.receive);
     BotFramework.controller.createWebhookEndpoints(app, BotFramework.bot);
     console.log('BotFramework bot is live');
+    controller = BotFramework.controller;
     callActions(BotFramework.controller);
   }
 };
 
 
-var callActions = function(controller){
+var callActions = function (controller) {
   controller.hears('(.*)', ['direct_message', 'direct_mention', 'mention', 'message_received'], function (bot, message) {
-      bot.reply(message, { type: "typing" });
-      //console.log(message);
-      id = message.user; 
-      controller.storage.users.get(id, function(error, user_data){
-         if(user_data === null) {
-		controller.storage.users.save({id:message.user, name:message.address.user.name, channel:message.source}, function(err) {
-                        // console.log(err);
-                        console.log('SUCCESS!! user added to the table');
-                 });
-	 }
+    bot.reply(message, {
+      type: "typing"
+    });
+    //console.log(message);
+    id = message.user;
+    controller.storage.users.get(id, function (error, user_data) {
+      if (user_data === null) {
+        controller.storage.users.save({
+          id: message.user,
+          name: message.address.user.name,
+          channel: message.source
+        }, function (err) {
+          // console.log(err);
+          console.log('SUCCESS!! user added to the table');
+        });
+      }
 
-      });
+    });
 
 
-        //controller.storage.users.all(function(err, users) {
-         //       console.log("All users == "+JSON.stringify(users));
-       // });
+    // controller.storage.users.all(function (err, users) {
+    //   console.log("All users == " + JSON.stringify(users));
+    // });
 
 
-	//	controller.storage.user_session.all(function(err, users) {
-	//	console.log("All sessions == "+JSON.stringify(users));
-	//});
+    // controller.storage.user_session.all(function (err, users) {
+    //   console.log("All sessions == " + JSON.stringify(users));
+    // });
 
-      controller.storage.user_session.find({id : id}, function(error, user_session){
-	  if(user_session.length > 0) {
-	 	console.log("Session Found");
-	  }
-          else {
-	 	controller.storage.user_session.save({id : message.user});
-		console.log("New Session created");
+    controller.storage.user_session.find({
+      id: id
+    }, function (error, user_session) {
+      if (user_session.length > 0) {
+        console.log("Session Found "+JSON.stringify(user_session));
+        queryString = '';
+        Object.keys(user_session[0]).forEach(function(key) {
+          var element = user_session[0][key];
+          switch (key) {
+            case "product":
+              queryString += 'global_attr_article_type:' + element + ' AND ';
+              break;
+            case "size":
+              queryString += 'sizes:' + element + ' AND ';
+              break;
+            case "color":
+              queryString += 'global_attr_base_colour:' + element + ' AND ';
+              break;
+            case "brand":
+              queryString += 'global_attr_brand:' + element + ' AND ';
+              break;
+            case "gender":
+              queryString += 'gender_from_cms:' + element + ' AND ';
+              break;
+            default:
+              break;
+          }  
+        });
+
+        console.log('SWITCH 1 == '+ queryString);
+      } else {
+        controller.storage.user_session.save({
+          id: message.user
+        });
+        console.log("New Session created");
+      }
+    });
+
+
+    client.message(message.text, {})
+      .then((data) => {
+        var entities = data.entities;
+        botEngine(entities, controller, function (reply) {
+          // console.log(reply);
+          if(reply === 'Item  not Found') {
+            console.log("gb == "+global_product);
+            controller.storage.user_session.save({
+              id: id,
+              'product': global_product
+            });
           }
-      }); 
-
-      client.message(message.text, {})
-        .then((data) => {
-          var entities = data.entities;
-          botEngine(entities, controller, function(reply) {
-              // console.log(reply);
-              bot.reply(message, reply);
-          }) 
+          bot.reply(message, reply);
         })
-        .catch(console.error);          
-      });
+      })
+      .catch(console.error);
+  });
 
 }
 
-var botEngine = function(entities, controller, callback) {
+var botEngine = function (entities, controller, callback) {
   let queryObject = {};
-    for (var key in entities) {
-      var item = entities[key];
-      // console.log(key+" == "+JSON.stringify(item));
-      for (var key2 in item) {
-         // console.log("item2 val == "+JSON.stringify(item[key2]['value']));
-        queryObject[key] = item[key2]['value'];
-      }
+  for (var key in entities) {
+    var item = entities[key];
+    // console.log(key+" == "+JSON.stringify(item));
+    for (var key2 in item) {
+      // console.log("item2 val == "+JSON.stringify(item[key2]['value']));
+      queryObject[key] = item[key2]['value'];
     }
+  }
   // console.log("Query == "+JSON.stringify(queryObject));
-  var queryString = '';
   for (var i in queryObject) {
     if (queryObject.hasOwnProperty(i)) {
-     switch(i) {
-      case "intent":
-         break;
-      case "product": queryString += 'global_attr_article_type:'+ queryObject[i] + ' AND ';
-         break;
-      case "product_size": queryString += 'sizes:'+ queryObject[i] + ' AND ';
-         break;
-      case "product_color": queryString += 'global_attr_base_colour:'+ queryObject[i] + ' AND ';
-         break;
-      case "product_brand": queryString += 'global_attr_brand:'+ queryObject[i] + ' AND ';
-         break;
-      case "gender":queryString += 'gender_from_cms:'+ queryObject[i] + ' AND ';
-         break;
-      default: queryString += 'no match found';
-         break;
+      switch (i) {
+        case "intent":
+          break;
+        case "product":
+          queryString = 'global_attr_article_type:' + queryObject[i] + ' AND ';
+          updateSession('product', queryObject[i]);
+          break;
+        case "product_size":
+          queryString += 'sizes:' + queryObject[i] + ' AND ';
+          updateSession('size', queryObject[i]);
+          break;
+        case "product_color":
+          queryString += 'global_attr_base_colour:' + queryObject[i] + ' AND ';
+          updateSession('color', queryObject[i]);
+          break;
+        case "product_brand":
+          queryString += 'global_attr_brand:' + queryObject[i] + ' AND ';
+          updateSession('brand', queryObject[i]);
+          break;
+        case "gender":
+          queryString += 'gender_from_cms:' + queryObject[i] + ' AND ';
+          updateSession('gender', queryObject[i]);
+          break;
+        default:
+         // queryString += 'no match found';
+          break;
       }
     }
   }
-  if(queryString.length > 0) {
+  console.log('SWITCH 2 == '+ queryString);
+  if (queryString.length > 0) {
     queryString = encodeURIComponent(queryString.slice(0, -4));
-    const options = {  
+    const options = {
       method: 'GET',
-      uri: process.env.ELASTIC_HOST+'?q='+queryString,
+      uri: process.env.ELASTIC_HOST + '?q=' + queryString,
       json: true
     }
-  
+    console.log(options);
+
     var API = require('./data');
     API.call(options, callback);
-  }
-  else {
-    controller.storage.users.get(id , function(error , user){
-	callback("Hey "+user.name+", How can I help You?");
+  } else {
+    controller.storage.users.get(id, function (error, user) {
+      callback("Hey " + user.name + ", How can I help You?");
     });
     console.log("No Match Found!!!!")
   }
+}
+
+
+var updateSession = function (key, val) {
+
+  if (key === 'product') {
+    global_product = val;
+    controller.storage.user_session.save({
+      id: id,
+      'product': val
+    });
+
+  } else {
+    MongoClient.connect(url, function (err, db) {
+      assert.equal(null, err);
+      // console.log("Connected successfully to server");
+      var collection = db.collection('user_session');
+      var new_value = {};
+      new_value[key] = val;
+      try {
+        collection.findOneAndUpdate({
+          "id": id
+        }, {
+          $set: new_value
+        }, {
+          upsert: true,
+          returnNewDocument: true
+        });
+      } catch (e) {
+        print(e);
+      }
+    });
+
+  }
+
 
 }
