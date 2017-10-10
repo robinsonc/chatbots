d168351a1f9721e157ded37ef9fd27d97687457f
EC2 Default User <ec2-user@ip-172-31-40-203.ec2.internal>
2017-10-10 07:08:54 +0000

    Boolean Query Incorporated

--- src/app.js
+++ src/app.js
@@ -32,12 +32,11 @@ const client = new Wit({
   //logger: new log.Logger(log.DEBUG) // optional 
 });
 
-var mongoUtil = require( '../database/mongoUtils' );
+var mongoUtil = require('../database/mongoUtils');
 var db;
-mongoUtil.connectToServer( function( err ) {
+mongoUtil.connectToServer(function (err) {
   db = mongoUtil.getDb();
 });
-
 var Promise = require('promise');
 
 var id;
@@ -45,6 +44,11 @@ var controller;
 var queryString = '';
 var mboxParams = {};
 var global_product;
+var boolQuery = {};
+var must = [];
+var filter = [];
+var should = [];
+var must_not = [];
 
 module.exports = function (app) {
   if (process.env.USE_SLACK) {
@@ -78,9 +82,6 @@ var callActions = function (controller) {
       type: "typing"
     });
     //console.log(message);
-
-
-
     id = message.user;
     controller.storage.users.get(id, function (error, user_data) {
       if (user_data === null) {
@@ -110,7 +111,7 @@ var callActions = function (controller) {
     }, function (error, user_session) {
       if (user_session.length > 0) {
         console.log("USER SESSION already exists!!");
-        console.log("SESSION == "+JSON.stringify(user_session));
+        console.log("SESSION == " + JSON.stringify(user_session));
       } else {
         controller.storage.user_session.save({
           id: message.user
@@ -129,8 +130,8 @@ var callActions = function (controller) {
             controller.storage.user_session.save({
               id: id,
               'product': global_product
-            });  
-          } 
+            });
+          }
           bot.reply(message, reply);
         })
       })
@@ -140,198 +141,282 @@ var callActions = function (controller) {
 }
 
 var botEngine = function (entities, controller, callback) {
-  let queryObject = {};
-  for (var key in entities) {
-    var item = entities[key];
-    // console.log(key+" == "+JSON.stringify(item));
-    for (var key2 in item) {
-      // console.log("item2 val == "+JSON.stringify(item[key2]['value']));
-      queryObject[key] = item[key2]['value'];
-    }
-  }
-  var promises = [];
-  for (var i in queryObject) {
-    if (queryObject.hasOwnProperty(i)) {
-      switch (i) {
-        case "intent":
-          break;
-        case "product":
-          promises.push(updateSession('product', queryObject[i]));
-          break;
-        case "product_size":
-          promises.push(updateSession('size', queryObject[i]));
-          break;
-        case "product_color":
-          promises.push(updateSession('color', queryObject[i]));
-          break;
-        case "product_brand":
-          promises.push(updateSession('brand', queryObject[i]));
-          break;
-        case "gender":
-          promises.push(updateSession('gender', queryObject[i]));
-          break;
-        default:
-          // queryString += 'no match found';
-          break;
+    let queryObject = {};
+    console.log("ENTITIES == "+JSON.stringify(entities));
+    for (var key in entities) {
+      var item = entities[key];
+      // console.log(key+" == "+JSON.stringify(item));
+      for (var key2 in item) {
+        // console.log("item2 val == "+JSON.stringify(item[key2]['value']));
+        queryObject[key] = item[key2]['value'];
       }
     }
-  }
-
-  Promise.all(promises)
-    .then(function (data) { // do stuff when success
-      // console.log(data);
-      createQuery(function () {
-        if (queryString.length > 0) {
-          queryString = encodeURIComponent(queryString.slice(0, -4));
-          console.log("QS == " + queryString);
-          const options = {
-            method: 'GET',
-            uri: process.env.ELASTIC_HOST + '?q=' + queryString,
-            json: true
-          }
-          console.log('mbox == '+ JSON.stringify(mboxParams));
-          const targetOptions = {
-            method: 'POST',
-            uri: 'https://adobeinternalags308.tt.omtrdc.net/rest/v1/mbox/123468?client=adobeinternalags308',
-            body: {
-              "mbox" : "chatbot_recommendation_mbox",
-              "tntId":"3bdfd9df079d45388dea33863a18b7f7.22_30",
-              "mboxParameters": mboxParams
-            },
-            json: true // Automatically stringifies the body to JSON
-          };
-          // console.log(targetOptions);
-          var API = require('../src/data');
-          API.call(options, callback);
-          var isTarget = API.target(targetOptions);
-          isTarget.then(function (data) {
-            // console.log(data);
-            setTimeout(function () {
-              API.display(data, callback);
-            }, 3000);
-            
-            }, function (err) {
-              console.error(err);
-          });
-          
-        } else {
-          controller.storage.users.get(id, function (error, user) {
-            var greet = "Hey" + (user.name === null ? '':' '+user.name) + ", How can I help You?";
-            callback(greet);         
-          });
-          console.log("No Match Found!!!!");
-        }
-      });
-    })
-    .catch(function (err) { // error handling
-      console.log(err);
-    });
-}
-
-
-var updateSession = function (key, val) {
-
-  return new Promise(function (resolve, reject) {
-    if (key === 'product') {
-      global_product = val;
-      //Store previous session if any 
-      controller.storage.user_session.get(id, function (error, user_data) {
-        // console.log("user session == "+ JSON.stringify(user_data), Object.keys(user_data).length);
-        if (Object.keys(user_data).length > 2) {
-          var collection = db.collection('user_history');
-          try {
-            var data = collection.insert({
-              "id": id,
-              user_data,
-              "ts": new Date()
-            });
-          } catch (e) {
-            console.log(e);
-          }
-          // controller.storage.user_history.find({id:id}, function (error, user_history) {
-          //   console.log('History == '+ JSON.stringify(user_history));
-          // });
-        }
-      });
-      //Create a new session
-      controller.storage.user_session.save({
-        id: id,
-        'product': val
-      });
-      resolve(true);
-
-    } else {
-        var collection = db.collection('user_session');
-        var new_value = {};
-        new_value[key] = val;
-        try {
-          var data = collection.findOneAndUpdate({
-            "id": id
-          }, {
-            $set: new_value
-          }, {
-            upsert: true,
-            returnNewDocument: false
-          }, function () {
-            resolve(true);
-          });
-        } catch (e) {
-          console.log(e);
-          reject(e);
-        }
-    }
-  })
-}
-
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-var createQuery = function (callback) {
-  controller.storage.user_session.find({
-    id: id
-  }, function (error, user_session) {
-    
-    if (user_session.length > 0) {
-      // console.log("Session Found " + JSON.stringify(user_session));
-      mboxParams = {              
-        brand:"",
-        color:"",
-        category:""
-      };
-      Object.keys(user_session[0]).forEach(function (key) {
-        var element = user_session[0][key];
-        switch (key) {
+    var promises = [];
+    for (var i in queryObject) {
+      if (queryObject.hasOwnProperty(i)) {
+        switch (i) {
           case "intent":
             break;
           case "product":
-            queryString = 'global_attr_article_type:' + element + ' AND ';
-            mboxParams.category = capitalizeFirstLetter(element);
+            promises.push(updateSession('product', queryObject[i]));
             break;
-          case "size":
-            queryString += 'sizes:' + element + ' AND ';
+          case "product_size":
+            promises.push(updateSession('size', queryObject[i]));
             break;
-          case "color":
-            queryString += 'global_attr_base_colour:' + element + ' AND ';
-            mboxParams.color = capitalizeFirstLetter(element);
+          case "product_color":
+            promises.push(updateSession('color', queryObject[i]));
             break;
-          case "brand":
-            queryString += 'global_attr_brand:' + element + ' AND ';
-            mboxParams.brand = capitalizeFirstLetter(element);
+          case "product_brand":
+            promises.push(updateSession('brand', queryObject[i]));
             break;
           case "gender":
-            queryString += 'gender_from_cms:' + element + ' AND ';
+            promises.push(updateSession('gender', queryObject[i]));
             break;
           default:
+            // queryString += 'no match found';
             break;
         }
-      });
-      callback();
-    } else {
-      controller.storage.user_session.save({
-        id: message.user
-      });
-      console.log("New Session created");
+      }
     }
-  });
-}
+
+    Promise.all(promises)
+      .then(function (data) { // do stuff when success
+          // console.log(data);
+          createQuery(function () {
+              if (queryString.length > 0) {
+                queryString = encodeURIComponent(queryString.slice(0, -4));
+                console.log("QS == " + queryString);
+                const options = {
+                  method: 'GET',
+                  uri: process.env.ELASTIC_HOST + '?q=' + queryString,
+                  json: true
+                }
+                boolQuery.must = must;
+                boolQuery.filter = filter;
+                boolQuery.should = should;
+                console.log("BOOL == " + JSON.stringify(boolQuery));
+                const elasticOptions = {
+                  method: 'GET',
+                  uri: process.env.ELASTIC_HOST,
+                  //   body: {
+                  //     "query": {
+                  //       "bool": {
+                  //         "must": [{
+                  //           "match": {
+                  //             "global_attr_article_type": "Tshirts"
+                  //           }
+                  //         }, {
+                  //           "match": {
+                  //             "gender_from_cms": "Men"
+                  //           }
+                  //         }],
+                  //         "filter": {
+                  //           "term": {
+                  //             "sizes": "S"
+                  //           }
+                  //         },
+                  //         "should": [{
+                  //           "term": {
+                  //             "global_attr_base_colour": "blue"
+                  //           }
+                  //         }, {
+                  //           "term": {
+                  //             "product_additional_info": "polo"
+                  //           }
+                  //         }],
+                  //         "must_not": {
+                  //           "range": {
+                  //             "price": {
+                  //               "gte": 2000,
+                  //               "lte": 1000
+                  //             }
+                  //           }
+                  //         }
+                  //       }
+                  //     }
+                  // },
+                  body: {
+                    "query": {
+                      "bool": boolQuery
+                    }
+                  },
+                    json: true // Automatically stringifies the body to JSON
+                };
+
+
+                  console.log('mbox == ' + JSON.stringify(mboxParams));
+                  const targetOptions = {
+                    method: 'POST',
+                    uri: 'https://adobeinternalags308.tt.omtrdc.net/rest/v1/mbox/123468?client=adobeinternalags308',
+                    body: {
+                      "mbox": "chatbot_recommendation_mbox",
+                      "tntId": "3bdfd9df079d45388dea33863a18b7f7.22_30",
+                      "mboxParameters": mboxParams
+                    },
+                    json: true // Automatically stringifies the body to JSON
+                  };
+                  // console.log(targetOptions);
+                  var API = require('../src/data');
+                  API.call(elasticOptions, callback);
+                  var isTarget = API.target(targetOptions);
+                  isTarget.then(function (data) {
+                    // console.log(data);
+                    setTimeout(function () {
+                      API.display(data, callback);
+                    }, 3000);
+
+                  }, function (err) {
+                    console.error(err);
+                  });
+
+                }
+                else {
+                  controller.storage.users.get(id, function (error, user) {
+                    var greet = "Hey" + (user.name === null ? '' : ' ' + user.name) + ", How can I help You?";
+                    callback(greet);
+                  });
+                  console.log("No Match Found!!!!");
+                }
+              });
+          })
+        .catch(function (err) { // error handling
+          console.log(err);
+        });
+      }
+
+
+    var updateSession = function (key, val) {
+
+      return new Promise(function (resolve, reject) {
+        if (key === 'product') {
+          global_product = val;
+          //Store previous session if any 
+          controller.storage.user_session.get(id, function (error, user_data) {
+            // console.log("user session == "+ JSON.stringify(user_data), Object.keys(user_data).length);
+            if (Object.keys(user_data).length > 2) {
+              var collection = db.collection('user_history');
+              try {
+                var data = collection.insert({
+                  "id": id,
+                  user_data,
+                  "ts": new Date()
+                });
+              } catch (e) {
+                console.log(e);
+              }
+              // controller.storage.user_history.find({id:id}, function (error, user_history) {
+              //   console.log('History == '+ JSON.stringify(user_history));
+              // });
+            }
+          });
+          //Create a new session
+          controller.storage.user_session.save({
+            id: id,
+            'product': val
+          });
+          resolve(true);
+
+        } else {
+          var collection = db.collection('user_session');
+          var new_value = {};
+          new_value[key] = val;
+          try {
+            var data = collection.findOneAndUpdate({
+              "id": id
+            }, {
+              $set: new_value
+            }, {
+              upsert: true,
+              returnNewDocument: false
+            }, function () {
+              resolve(true);
+            });
+          } catch (e) {
+            console.log(e);
+            reject(e);
+          }
+        }
+      })
+    }
+
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    var createQuery = function (callback) {
+      controller.storage.user_session.find({
+        id: id
+      }, function (error, user_session) {
+
+        if (user_session.length > 0) {
+          mboxParams = {
+            brand: "",
+            color: "",
+            category: ""
+          };
+          boolQuery = {};
+          must = [];
+          filter = [];
+          should = [];
+          must_not = [];
+          Object.keys(user_session[0]).forEach(function (key) {
+            var element = user_session[0][key];
+            switch (key) {
+              case "intent":
+                break;
+              case "product":
+                must.push({
+                  "match": {
+                    "global_attr_article_type": element
+                  }
+                })
+                queryString = 'global_attr_article_type:' + element + ' AND ';
+                mboxParams.category = capitalizeFirstLetter(element);
+                break;
+              case "size":
+                filter.push({
+                  "term": {
+                    "sizes": element
+                  }
+                })
+                queryString += 'sizes:' + element + ' AND ';
+                break;
+              case "color":
+                should.push({
+                  "match": {
+                    "global_attr_base_colour": element
+                  }
+                })
+                queryString += 'global_attr_base_colour:' + element + ' AND ';
+                mboxParams.color = capitalizeFirstLetter(element);
+                break;
+              case "brand":
+                should.push({
+                  "match": {
+                    "global_attr_brand": element
+                  }
+                })
+                queryString += 'global_attr_brand:' + element + ' AND ';
+                mboxParams.brand = capitalizeFirstLetter(element);
+                break;
+              case "gender":
+                must.push({
+                  "match": {
+                    "gender_from_cms": element
+                  }
+                })
+                queryString += 'gender_from_cms:' + element + ' AND ';
+                break;
+              default:
+                break;
+            }
+          });
+          callback();
+        } else {
+          controller.storage.user_session.save({
+            id: message.user
+          });
+          console.log("New Session created");
+        }
+      });
+    }
